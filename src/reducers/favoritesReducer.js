d033ecd397254fedd51628ac976ce61f103e714c
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-02-15 18:21:13 -0600

    favorites reducer built

---
+++ src/reducers/favoritesReducer.js
@@ -0,0 +1,15 @@
+
+
+const initialState = {
+    favorites: [], 
+    displayFavorites: true
+}
+
+const favoritesReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+        return state
+    }
+}
+
+export default favoritesReducer; 
